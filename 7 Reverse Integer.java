1b8f24258a6f50b4cbf124df922b70596205ba3d
zhuwr <wrzhu@buaa.edu.cn>
2015-06-14 08:47:17 +0800

    Create 7 Reverse Integer.java
    Easy

---
+++ 7 Reverse Integer.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int reverse(int x) {
+        int ret = 0;
+        
+        while(x != 0) {
+            int next_ret = 10*ret + x%10;
+            x = x/10;
+            if(next_ret/10 != ret) {
+                return 0;
+            }
+            ret = next_ret;
+        }
+        return ret;
+    }
+}
