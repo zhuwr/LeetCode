9ecd40e93f2c5ab64f54b901a6e0a1881518875a
zhuwr <wrzhu@buaa.edu.cn>
2015-06-16 15:23:21 +0800

    Update 189 Rotate Array.java
    Easy

--- 189 Rotate Array.java
+++ 189 Rotate Array.java
@@ -5,17 +5,17 @@ public class Solution {
         }
         int length = nums.length;
         k = k%length;
-       int[] temp = new int[k];
-       int i;
-       for(i=0; i<k; i++) {
+        int[] temp = new int[k];
+        int i;
+        for(i=0; i<k; i++) {
            temp[i] = nums[length-k+i];
-       }
-       for(i=length-k-1; i>=0; i--) {
-           nums[i+k] = nums[i];
-       }
-       for(i=0; i<k; i++) {
+        }
+        for(i=length-k-1; i>=0; i--) {
+            nums[i+k] = nums[i];
+        }
+        for(i=0; i<k; i++) {
            nums[i] = temp[i];
-       }
+        }
         
     }
 }
