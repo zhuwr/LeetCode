dce202ede0ba2543d3b00528c0884efe340be029
zhuwr <wrzhu@buaa.edu.cn>
2015-06-16 15:02:29 +0800

    Create 6 ZigZag Conversion.java

---
+++ 6 ZigZag Conversion.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1 || s.length()<numRows) {
+            return s;
+        }
+        char[] Rows =new char[s.length()];
+        int count = 0;
+        int step = 2*(numRows-1);
+        for(int i=0; i<numRows; i++) {
+            int interval = step - 2*i;
+            for(int j=i; j<s.length(); j+=step) {
+                Rows[count] = s.charAt(j);
+                count++;
+                if(interval>0 && interval<step&&j+interval<s.length()) {
+                    Rows[count] = s.charAt(j+interval);
+                    count++;
+                }
+            }
+        }
+
+        return new String(Rows);
+        
+    }
+}
