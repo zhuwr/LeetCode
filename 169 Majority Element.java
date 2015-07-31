a5aec51006467f8cbba7e734782a5c699bfaf40c
zhuwr <wrzhu@buaa.edu.cn>
2015-07-31 15:50:47 +0800

    Create 169 Majority Element.java

---
+++ 169 Majority Element.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int Maj = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] == Maj) {
+                count++;
+            } else {
+                count--;
+            }
+            if(count == 0) {
+                Maj = nums[i];
+                count = 1;
+            }
+        }
+        return Maj;
+    }
+}
