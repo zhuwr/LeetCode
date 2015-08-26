7b7f1fd113762b01da10a46a5865dafa797c8277
zhuwr <wrzhu@buaa.edu.cn>
2015-08-26 16:43:17 +0800

    Medium

---
+++ 073 Set Matrix Zeroes.java
@@ -0,0 +1,60 @@
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        if(matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstm = false;
+        boolean firstn = false;
+        
+        for(int i=0; i<m; i++) {
+            if(matrix[i][0] == 0) {
+                firstm = true;
+                break;
+            }
+        }
+        for(int i=0; i<n; i++) {
+            if(matrix[0][i] == 0) {
+                firstn = true;
+                break;
+            }
+        }
+        
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i=1; i<m; i++) {
+            if(matrix[i][0] == 0) {
+                for(int j=1; j<n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i=1; i<n; i++) {
+            if(matrix[0][i] == 0) {
+                for(int j=1; j<m; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        
+        if(firstm) {
+            for(int i=0; i<m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if(firstn) {
+            for(int i=0; i<n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+    }
+}
