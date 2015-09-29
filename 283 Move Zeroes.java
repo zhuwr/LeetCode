136e7b7bd49a549cdccc1ca9e4d07d45b6280909
zhuwr <wrzhu@buaa.edu.cn>
2015-09-29 16:21:53 +0800

    Easy

---
+++ 283 Move Zeroes.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 0) {
+                count++;
+            } else {
+                nums[i-count] = nums[i];
+            }
+        }
+        for(int i=nums.length-count; i<nums.length; i++) {
+            nums[i] = 0;
+        }
+        
+    }
+}
